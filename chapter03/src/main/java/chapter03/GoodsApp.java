bec811f8dd6efc9e6a94ef36971da93666c9261e
sjgmajor <sjg3234@gmail.com>
2023-08-09 11:22:03 +0900

    init commit

--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -4,8 +4,15 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		Goods camera = new Goods();
+		camera.name = "Nikon";
+		camera.price = 400000;
+		camera.countStock = 10;
+		camera.countSold = 20;
 		
-
+		System.out.println(
+				"상품이름: " + camera.name + 
+				", 가격: " + camera.price + 
+				", 재고개수: " + camera.countStock +
+				", 팔린 개수: " + camera.countSold );
 	}
-
 }
