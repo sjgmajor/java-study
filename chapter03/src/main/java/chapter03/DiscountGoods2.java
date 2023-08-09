e0483d17e074943dad37c17b68a75a9aac960d2a
sjgmajor <sjg3234@gmail.com>
2023-08-09 12:50:10 +0900

    init commit

--- chapter03/src/main/java/chapter03/DiscountGoods2.java
+++ chapter03/src/main/java/chapter03/DiscountGoods2.java
@@ -7,9 +7,7 @@ public class DiscountGoods2 extends Goods2{
 	private float discountRate = 0.5f;
 	
 	public int getDiscountPrice() {
-	
 	      int discountPrice = (int) (discountRate * price) ;
 	      return discountPrice;
 	}
-
 }
--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -4,15 +4,30 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		Goods camera = new Goods();
-		camera.name = "Nikon";
-		camera.price = 400000;
-		camera.countStock = 10;
-		camera.countSold = 20;
+		//countOfGoods = 1
+		
+		camera.setName("Nikon");
+		camera.setPrice(400000);
+		camera.setCountStock(10);
+		camera.setCountSold(20);
+		
+		// 정보은닉(데이터 보호) -> setprice 매서드로 데이터 오류를 방지할 수 있음
+		camera.setPrice(-1);
+		
+		// countOfGoods 테스트
+		Goods goods1 = new Goods();
+		//countOfGoods = 2
+		Goods goods2 = new Goods();
+		//countOfGoods = 3
+		Goods goods3 = new Goods();
+		//countOfGoods = 4
+		
+		System.out.println(Goods.countOfGoods);
 		
 		System.out.println(
-				"상품이름: " + camera.name + 
-				", 가격: " + camera.price + 
-				", 재고개수: " + camera.countStock +
-				", 팔린 개수: " + camera.countSold );
+				"상품이름: " + camera.getName() + 
+				", 가격: " + camera.getPrice() + 
+				", 재고개수: " + camera.getCountStock() +
+				", 팔린 개수: " + camera.getCountSold() );
 	}
 }
