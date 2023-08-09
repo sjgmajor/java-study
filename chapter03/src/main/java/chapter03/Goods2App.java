1cbbebde6d0f6540270e9543ee2c211a5183e78a
sjgmajor <sjg3234@gmail.com>
2023-08-09 12:05:05 +0900

    init commit

--- chapter03/src/main/java/chapter03/Goods2App.java
+++ chapter03/src/main/java/chapter03/Goods2App.java
@@ -1,18 +1,25 @@
 package chapter03;
 
+import mypackage.Goods2;
+
 public class Goods2App {
 
 	public static void main(String[] args) {
-		Goods camera = new Goods();
-		camera.name = "Nikon";
-		camera.price = 400000;
-		camera.countStock = 10;
-		camera.countSold = 20;
+		Goods2 goods = new Goods2();
 		
-		System.out.println(
-				"상품이름: " + camera.name + 
-				", 가격: " + camera.price + 
-				", 재고개수: " + camera.countStock +
-				", 팔린 개수: " + camera.countSold );
+		// public: 접근 제한이 없다.
+		goods.name = "camera";
+		
+		// protected: 같은 패키지에서 접근이 가능하다.(오류)
+		//            더 중요한 접근 제어는 자식에서 접근 가능하다
+		// goods.price = 10000;
+		
+		// 디폴트는 같은 패키지에서 접근이 가능하다.(오류)
+		// goods.countStock = 10;
+		
+		// private는 같은 클래스에서만 접근이 가능하다.(오류)
+		// goods.countSold = 20;
+		
 	}
 }
+
