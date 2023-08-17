1d59e70545eb48255cf6cb5c1fa677a65123ef49
서정권 <sjg3234@gmail.com>
2023-08-17 09:40:59 +0900

    generic & parameter

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,20 +1,40 @@
 package prob01;
 
 public class Printer {
+//
+//	public void println(int number) {
+//		System.out.println(number);
+//	}
+//	
+//	public void println(boolean bool) {
+//		System.out.println(bool);
+//	}
+//	
+//	public void println(double doubleNumber) {
+//		System.out.println(doubleNumber);
+//	}
+//	
+//	public void println(String string) {
+//		System.out.println(string);
+//	}
+	
+//	public <T> void println(T t) {
+//		System.out.println(t);
+//	}
+//	
+	public int sum(Integer...nums) {
+		int s = 0;
+		for(Integer i : nums) {
+			s += i;
+		}
+		return s;
+	}
 
-	public void println(int number) {
-		System.out.println(number);
+	public <T> void println(T... ts) {
+		for( T t : ts) {
+		System.out.println(t);
+		}
 	}
+
 	
-	public void println(boolean bool) {
-		System.out.println(bool);
-	}
-	
-	public void println(double doubleNumber) {
-		System.out.println(doubleNumber);
-	}
-	
-	public void println(String string) {
-		System.out.println(string);
-	}
 }
